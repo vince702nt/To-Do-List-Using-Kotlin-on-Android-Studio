558ee2b99d804ff1f60ff7aabde328e24e44dd39
vince702nt <vincenwangwu@gmail.com>
2025-08-17 10:57:51 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/to_do_list/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.to_do_list;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.to_do_list.Adapter.ToDoAdapter;
+import com.example.to_do_list.Model.ToDoModel;
+import com.example.to_do_list.Utils.DataBaseHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnDialogCloseListener{
+
+    RecyclerView recyclerView;
+    FloatingActionButton addButton;
+    DataBaseHelper myDB;
+    private List<ToDoModel> mList;
+    private ToDoAdapter adapter;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        addButton = findViewById(R.id.addButton);
+        myDB = new DataBaseHelper (MainActivity.this);
+        mList = new ArrayList<>();
+        adapter = new ToDoAdapter(myDB, MainActivity.this);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        mList = myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AddNewTask.newInstance().show(getSupportFragmentManager(), AddNewTask.TAG);
+            }
+        });
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerViewTouchHelper(adapter));
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+
+    }
+
+    @Override
+    public void onDialogClose(DialogInterface dialogInterface) {
+        mList = myDB.getAllTasks();
+        Collections.reverse(mList);
+        adapter.setTasks(mList);
+        adapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/example/to_do_list/OnDialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.to_do_list;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+    void onDialogClose(DialogInterface dialogInterface);
+}
